e1faf35436b1c1421c601f48566a6b3757f5c376
Bruno Lima <102358503+bhlx1992@users.noreply.github.com>
2022-05-05 20:04:45 +0200

    Add files via upload

---
+++ Main10.java
@@ -0,0 +1,121 @@
+public class Main10 {
+
+	public static void main(String[] args) {			
+		final String blue = "\u001B[44m";
+		final String red = "\u001B[41m";
+		final String yellow = "\u001B[43m";
+		final String white = "\u001B[47m";
+		final String black = "\u001B[40m";	
+		final String green = "\u001B[42m";
+		final String space = "                          ";
+		final String space2 = "                                     ";
+		final String space4 = "                          ";
+		final String space3 = "            ";
+		final String eGrecia = "    ";
+		final String eGrecia2 = "   ";	
+		final String eGrecia3 = "           ";
+
+//Ukraine
+	System.out.println("");
+	System.out.println(black+"Ukraine");			
+	System.out.println(blue+space2);
+	System.out.println(blue+space2);
+	System.out.println(blue+space2);
+	System.out.println(blue+space2);
+	System.out.println(blue+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println("");
+
+//GRECIA
+	System.out.println(black+"Greece");
+	System.out.print(blue+eGrecia+white+eGrecia2+blue+eGrecia);
+	System.out.println(blue+space);
+	System.out.print(blue+eGrecia+white+eGrecia2+blue+eGrecia);
+	System.out.println(white+space4);
+	System.out.println(white+eGrecia3+blue+space4);
+	System.out.print(blue+eGrecia+white+eGrecia2+blue+eGrecia);
+	System.out.println(white+space4);
+	System.out.print(blue+eGrecia+white+eGrecia2+blue+eGrecia);
+	System.out.println(blue+space);
+	
+	System.out.println(white+space2);
+	System.out.println(blue+space2);
+	System.out.println(white+space2);
+	System.out.println(blue+space2); 
+	System.out.println("");
+		
+//FRANCE	
+	System.out.println(black+"France");
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println(blue+space3+white+space3+red+space3);
+	System.out.println("");
+	
+//SPAIN	
+	System.out.println(black+"Spain");
+	System.out.println(red+space2);
+	System.out.println(red+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println(red+space2);
+	System.out.println(red+space2);
+	System.out.println("");
+	
+//CATALUNYA
+	System.out.println(black+"Catalunya");
+	System.out.println(yellow+space2);
+	System.out.println(red+space2);
+	System.out.println(yellow+space2);
+	System.out.println(red+space2);
+	System.out.println(yellow+space2);
+	System.out.println(red+space2);
+	System.out.println(yellow+space2);
+	System.out.println(red+space2);
+	System.out.println(yellow+space2);
+	System.out.println("");
+	
+//GERMANY
+	System.out.println(black+"Germany");
+	System.out.println(black+space2);
+	System.out.println(black+space2);
+	System.out.println(black+space2);
+	System.out.println(red+space2);
+	System.out.println(red+space2);
+	System.out.println(red+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	System.out.println(yellow+space2);
+	
+//ITALY	
+	System.out.println(black+"Italy");
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println(green+space3+white+space3+red+space3);
+	System.out.println("");
+	
+		
+			
+			
+	}
+			
+		
+
+}
